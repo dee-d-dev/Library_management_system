3762baad546c4bcc631bef316bc4831a45014c58
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-07 10:23:41 +0100

    Generating a jwt for User authorization

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -1,4 +1,5 @@
-const User = require("../models/User");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const registerUser = (req, res) => {
   const user = new User(req.body);
@@ -7,11 +8,18 @@ const registerUser = (req, res) => {
     .save()
     .then((user) => {
       console.log(user);
-      res.send(user);
+      res.status(201).send("account succesfully created");
     })
     .catch((err) => {
       console.log(err);
     });
+
+  //create token
+  const token = jwt.sign({ data: user.name }, process.env.TOKEN_KEY, {
+    expiresIn: 500,
+  });
+
+  user.token = token;
 };
 
 module.exports = registerUser;
--- models/User.js
+++ models/User.js
@@ -6,21 +6,9 @@ const User_Schema = new mongoose.Schema({
   age: Number,
   password: String,
   dateCreated: { type: Date, default: Date.now() },
+  token: String
 });
 
 const User = mongoose.model("User", User_Schema);
 
-// const user = new User({
-//   name: "user three",
-//   role: "Librarian",
-//   emaiil: "userone@gmail.com",
-//   age: 45,
-//   password: "4567",
-// });
-
-// const result = user.save((err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-// });
 module.exports = User;
