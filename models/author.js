f1aa0541fa5f5e9fcf924be77b390a247769d2ca
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-08 23:05:57 +0100

    fixed models

--- models/author.js
+++ models/author.js
@@ -2,5 +2,9 @@ const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
-  name: { type: String, required: true  },
+  name: { type: String, required: true },
 });
+
+const Author = mongoose.model("author", authorSchema);
+
+module.exports = Author;
