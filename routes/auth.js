928b503fc72c18fc492d9250ab6cc60e947c7647
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-09 22:06:33 +0100

    fixing the protected routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,17 @@ const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
 
 const verifyAccessToken = (req, res, next) => {
-  if (!req.headers["authorization"]) {
+  if (!req.header("authorization")) {
     return next(createError.Unauthorized());
   }
 
-  bearerToken = req.headers["authorization"];
+  bearerToken = req.header("authorization");
   jwt.verify(bearerToken, process.env.TOKEN_KEY, (err, data) => {
     if (err) {
-      return next(createError.Unauthorized());
+      return next(createError.Unauthorized("You cannot access this route"));
     }
-    req.data = data;
+    req.user = data;
+    next();
     // console.log(decoded.data);
   });
 };
