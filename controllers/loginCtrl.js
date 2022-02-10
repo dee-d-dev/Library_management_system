1c0730f0d011943b54ae38635c33adaf032c7eea
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-10 20:50:09 +0100

    fixed protected route

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -9,27 +9,24 @@ const LoginCtrl = async (req, res) => {
   }
 
   const user = await User.findOne({ email });
-  if (user == null) {
-    res.send("cannot find user");
-  }
+  if (!user) return res.send("cannot find user");
 
-  if (await bcrypt.compare(password, user.password)) {
-    if (user.role == "librarian") {
-      const token = jwt.sign(
-        { data: user.name, iss: "adedotun" },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
+  let authorized = await bcrypt.compare(password, user.password);
+  if (!authorized) res.send("incorrect email or password");
 
-      user.token = token;
-      res.header("bearer", token).send({ role: user.role, token: token });
-    }
+  if (user.role !== "librarian")
     return res.send({ role: user.role, token: user.token });
-  } else {
-    res.send("incorrect email or password");
-  }
+
+  const token = jwt.sign(
+    { data: user.name, iss: "adedotun" },
+    process.env.TOKEN_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  user.token = token;
+  res.header("bearer", token).send(token);
 };
 
 module.exports = LoginCtrl;
