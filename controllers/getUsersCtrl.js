48d6ff18fb7c7e6bb6047303c483c4b0ceac9f48
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-12 12:33:33 +0100

    Give access to librarian to get all users

--- controllers/getUsersCtrl.js
+++ controllers/getUsersCtrl.js
@@ -1,5 +1,9 @@
-const getUsers = (req, res) => {
-  res.send("Get Users");
+const User = require("../models/user");
+
+const getUsers = async (req, res) => {
+  const users = await User.find().select("-_id name role email bookIssued");
+
+  res.send(users);
 };
 
 module.exports = getUsers;
--- routes/getUsers.js
+++ routes/getUsers.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const getUsers = require("../controllers/getUsersCtrl");
 const verifyAccessToken = require("./auth");
 
-router.get("/all", verifyAccessToken, getUsers);
-
-module.exports = router;
+router.get("/all",  getUsers);
+// verifyAccessToken, 
+(module.exports = router);
