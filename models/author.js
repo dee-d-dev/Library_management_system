16f5944af9d1cc9a93320e6fd4bd78653327a729
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-12 17:08:50 +0100

    created a model for authr

--- models/author.js
+++ models/author.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const authorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   books: { type: [String], required: true },
+  email: { type: String, required: true },
+  quantity: { type: Number, required: true },
 });
 
 const Author = mongoose.model("author", authorSchema);
