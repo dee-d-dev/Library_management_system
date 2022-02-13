207a9f0730f43b6a8026313b04b55792d54d04d7
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-13 23:06:14 +0100

    updated the Author query

--- controllers/getAuthorCtrl.js
+++ controllers/getAuthorCtrl.js
@@ -1,8 +1,9 @@
 const Author = require("../models/author");
 
-const getAuthor= async (req, res) => {
-  const author = await Author.findById(req.params.id)
-  res.send(author)
+const getAuthor = async (req, res) => {
+  const author = await Author.findById(req.params.id).select('-_id name email books quantity');
+  // if (!author) return res.status(400).send("the author id input is wrong");
+  return res.send(author);
 };
 
 module.exports = getAuthor;
