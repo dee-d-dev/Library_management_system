4d93cc2d890c7a509c6cb35ed5ce39ea0dafc926
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-13 23:55:08 +0100

    populating the model with the the relationship model

--- controllers/getBookCtrl.js
+++ controllers/getBookCtrl.js
@@ -1,8 +1,10 @@
 const Book = require("../models/book");
 
 const getBook = async (req, res) => {
-  const { name } = req.body;
-  const book = await Book.findOne({ name });
+  const { title } = req.body;
+  const book = await Book.findOne({ title })
+    .populate("author")
+    .select("title author");
 
   if (!book) res.status(400).send("Sorry, We cannot find book in the Library");
 
