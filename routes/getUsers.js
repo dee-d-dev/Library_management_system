900c5a497b68b7ac951fc817bc862a9630f4dcf6
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-14 23:40:07 +0100

    protected the get all user route

--- routes/getUsers.js
+++ routes/getUsers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const getUsers = require("../controllers/getUsersCtrl");
 const verifyAccessToken = require("./auth");
 
-router.get("/all", getUsers);
+router.get("/all", verifyAccessToken, getUsers);
 
 // verifyAccessToken,
 module.exports = router;
