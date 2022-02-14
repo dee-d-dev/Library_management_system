bfc4672ffaafa0a62db8b33d0d5aec83c07c3439
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-14 19:04:16 +0100

    reduced the query gotten from the populated model

--- controllers/getBookCtrl.js
+++ controllers/getBookCtrl.js
@@ -4,7 +4,7 @@ const Author = require("../models/author");
 const getBook = async (req, res) => {
   const { title } = req.body;
   const book = await Book.findOne({ title })
-    .populate("author")
+    .populate("author", 'name email -_id')
     .select("title author");
 
   if (!book) res.status(400).send("Sorry, We cannot find book in the Library");
