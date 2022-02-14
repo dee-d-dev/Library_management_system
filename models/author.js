2f601272a63091ec4ee4f635cd213027a9dba9a0
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-14 23:47:54 +0100

    created a relationship between the author and the book docuemnts"

--- models/author.js
+++ models/author.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
   name: { type: String, lowercase: true, required: true },
-  books: { type: [String], lowercase: true, required: true },
+  books: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "book",
+    lowercase: true,
+    required: true,
+  },
   email: { type: String, lowercase: true, required: true },
   quantity: { type: Number, required: true },
 });
