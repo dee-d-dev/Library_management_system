0cfd7d6ef0e0174e58aa398b9f90ab8f55389aaa
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-25 08:07:57 +0100

    writing test for each route

--- tests/book.test.js
+++ tests/book.test.js
@@ -1,1 +1,12 @@
-test("Hi", () => {});
+const createBook = require("../controllers/createBook");
+const request = require('supertest');
+
+
+describe('To test the update book route', () => {
+    it('should contain title and quantity', () => {
+        request(createBook).post('/create/book').expect((res)=> {
+            res.body.title=res.body.title
+            res.body.quantity=res.body.quantity
+        })
+    })
+})
